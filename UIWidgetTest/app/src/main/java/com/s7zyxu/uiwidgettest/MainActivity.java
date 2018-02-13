c21eac63d859a6c7dfc11fd03f85bde2df5b559a
S7ZYXU <z9807060701@163.com>
2018-02-13 15:13:10 +0800

    3.2

--- UIWidgetTest/app/src/main/java/com/s7zyxu/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/s7zyxu/uiwidgettest/MainActivity.java
@@ -1,26 +1,39 @@
 package com.s7zyxu.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
-        final EditText editText = (EditText) findViewById(R.id.edit_text);
+        editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
-
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
             }
         });
     }
