3939cac5e9438c2fb790c4d3d7209b4b94fd3532
S7ZYXU <z9807060701@163.com>
2018-02-22 22:34:26 +0800

    3.5

--- UILayoutTest/app/src/main/java/com/s7zyxu/uilayouttest/MainActivity.java
+++ UILayoutTest/app/src/main/java/com/s7zyxu/uilayouttest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.s7zyxu.uilayouttest;
 
+import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
