515f0a6bd1ab17b33d1383147abcef564832dd1b
S7ZYXU <z9807060701@163.com>
2018-02-23 21:56:31 +0800

    ActivityTest

---
+++ ActivityTest/app/src/main/java/com/s7zyxu/activitytest/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.s7zyxu.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("SecondActivity","Tsak id is " + getTaskId());
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("SecondActivity","onDestroy");
+    }
+}
