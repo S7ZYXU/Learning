e9e8bf60cd2f50913bae313ee218ec4ed7527800
S7ZYXU <z9807060701@163.com>
2018-02-23 21:57:26 +0800

    UILayoutTest

---
+++ UILayoutTest/app/src/main/java/com/s7zyxu/uilayouttest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.s7zyxu.uilayouttest;
+
+import android.content.Context;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.hide();
+        }
+    }
+}
