1d928148339eab9439d5f44a80675ec41917396c
S7ZYXU <35862353+S7ZYXU@users.noreply.github.com>
2018-03-01 21:48:38 +0800

    Calculator

--- Calculator/app/src/main/java/com/s7zyxu/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/s7zyxu/calculator/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private TextView editView;
     private String op;
-    private double num1,num2,result;
+    private double num1=0,num2=0,result=0;
     private Button btnum0,btnum1,btnum2,btnum3,btnum4,btnum5,btnum6,btnum7,btnum8,btnum9;
     private Button btopposite,btdivision,btmulti,btsub,btadd,btresult;
     private Button btpoint;
@@ -76,78 +76,137 @@ public class MainActivity extends AppCompatActivity {
             try{
                 switch (button.getId()){
                     case R.id.num0:{
-
+                        if(!editView.getText().equals("0")){
+                            editView.setText(editView.getText().toString()+0);
+                        }
                         break;
                     }
                     case R.id.num1:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("1");
+                        }else{
+                            editView.setText(editView.getText().toString()+1);
+                        }
                         break;
                     }
                     case R.id.num2:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("2");
+                        }else{
+                            editView.setText(editView.getText().toString()+2);
+                        }
                         break;
                     }
                     case R.id.num3:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("3");
+                        }else{
+                            editView.setText(editView.getText().toString()+3);
+                        }
                         break;
                     }
                     case R.id.num4:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("4");
+                        }else{
+                            editView.setText(editView.getText().toString()+4);
+                        }
                         break;
                     }
                     case R.id.num5:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("5");
+                        }else{
+                            editView.setText(editView.getText().toString()+5);
+                        }
                         break;
                     }
                     case R.id.num6:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("6");
+                        }else{
+                            editView.setText(editView.getText().toString()+6);
+                        }
                         break;
                     }
                     case R.id.num7:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("7");
+                        }else{
+                            editView.setText(editView.getText().toString()+7);
+                        }
                         break;
                     }
                     case R.id.num8:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("8");
+                        }else{
+                            editView.setText(editView.getText().toString()+8);
+                        }
                         break;
                     }
                     case R.id.num9:{
-
+                        if(editView.getText().equals("0")){
+                            editView.setText("9");
+                        }else{
+                            editView.setText(editView.getText().toString()+9);
+                        }
                         break;
                     }
 
                     case R.id.opposite:{
-
+                        str = editView.getText().toString();
+                        if(Character.isDigit(str.charAt(str.length()-1))){
+                            double d = Double.valueOf(str);
+                            d = -d;
+                            editView.setText(String.valueOf(d));
+                        }
                         break;
                     }
                     case R.id.division:{
-
+                        str = editView.getText().toString();
+                        num1 = Double.valueOf(str);
+                        editView.setText(str+'/');
                         break;
                     }
                     case R.id.multi:{
-
+                        str = editView.getText().toString();
+                        num1 = Double.valueOf(str);
+                        editView.setText(str+'*');
                         break;
                     }
                     case R.id.sub:{
-
+                        str = editView.getText().toString();
+                        num1 = Double.valueOf(str);
+                        editView.setText(str+'-');
                         break;
                     }
                     case R.id.add:{
-
+                        str = editView.getText().toString();
+                        num1 = Double.valueOf(str);
+                        editView.setText(str+'+');
                         break;
                     }
 
                     case R.id.point:{
-
+                        str = editView.getText().toString();
+                        if(str.indexOf('.')==-1){
+                            editView.setText(str+'.');
+                        }
                         break;
                     }
 
                     case R.id.C:{
-
+                        textView.setText("");
+                        editView.setText(0);
+                        num1 = 0;
+                        num2 = 0;
+                        op = "+";
                         break;
                     }
                     case R.id.CE:{
-
+                        editView.setText(0);
+                        num2 = 0;
                         break;
                     }
 
